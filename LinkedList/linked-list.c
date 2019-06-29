75d87bf5b322f557a96077469f25de538f9b44d6
Kelly <kellbianca0@gmail.com>
2019-06-28 22:33:47 -0300

    Linked List

---
+++ LinkedList/linked-list.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+  int element;
+  struct node *next;
+}NODE;
+
+NODE *createList(int element)
+{
+  NODE *newNode = (NODE*) malloc(sizeof(NODE));
+  newNode->element = element;
+  newNode->next = NULL;
+  return newNode;
+}
+
+NODE *addListTail(NODE *node, int elements)
+{
+  NODE *aux = node;
+  if(aux == NULL)
+  {
+    return createList(elements);
+  }
+  aux->next = addListTail(aux->next, elements);
+  return aux;
+}
+
+NODE *addListHead(NODE *node, int element)
+{
+  NODE *aux = createList(element);
+  aux->next = node;
+  return aux;
+}
+
+void printListRecursive(NODE *node)
+{
+  NODE *aux = node;
+  if(aux != NULL)
+  {
+    printf("%d ",aux->element);
+    printListRecursive(aux->next);
+  }
+}
+
+void printList(NODE *node)
+{
+  NODE *aux = node;
+  while (aux->next != NULL)
+  {
+    printf("%d ",aux->element);
+    aux = aux->next;
+  }
+  printf("%d\n",aux->element);
+}
+
+NODE *searchRecursive(NODE *node, int element)
+{
+  NODE *aux = node;
+  if(aux != NULL)
+  {
+    if(aux->element == element)
+    {
+      return node;
+    }
+    else
+    {
+      return searchRecursive(aux->next, element);
+    }
+  }
+  return 0;
+}
+
+NODE *search(NODE *node, int element)
+{
+  NODE *aux = node;
+  while (aux != NULL)
+  {
+    if(aux->element == element)
+    {
+      return node;
+    }
+    aux = aux->next;
+  }
+  return 0;
+}
+
+int main()
+{
+  NODE *newNode = NULL;
+
+  int n, i, elements,s,r;
+  printf("Enter a quantity of elements in the list:\n");
+  scanf("%d",&n);
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d",&elements);
+    //adds elements at the top of the list
+    newNode = addListHead(newNode, elements);
+    //adds elements to the tail of the list
+    // newNode = addListTail(newNode, elements);
+  }
+  // printListRecursive(newNode);
+  printf("\n");
+
+  printf("Enter a number for the search\n");
+  scanf("%d",&s );
+
+  if(searchRecursive(newNode, s))
+  {
+    printf("The element was found in the list\n");
+  }
+  else
+  {
+    printf("The element is not in the list\n");
+  }
+
+  printf("The elements of the list: \n");
+  printList(newNode);
+  return 0;
+}
