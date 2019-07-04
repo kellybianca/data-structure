de7246ded17a637e82790ec932c0944f7d570d59
Kelly <kellbianca0@gmail.com>
2019-07-03 23:01:32 -0300

    Stack

--- Stack/stack.c
+++ Stack/stack.c
@@ -17,7 +17,7 @@ STACK *createStack()
   return new;
 }
 
-void push(STACK *stack, int element)
+int push(STACK *stack, int element)
 {
   NODE *new = (NODE*) malloc(sizeof(NODE));
   new->element = element;
@@ -25,25 +25,71 @@ void push(STACK *stack, int element)
   stack->top = new;
 }
 
-int is_empty(STACK *stack) {
-  if(stack->top == NULL) return 1;
+int is_empty(STACK *stack)
+{
+    return (stack->top == NULL);
 }
 
 int pop(STACK *stack)
 {
-  if(!is_empty(stack))
+  NODE *aux = stack->top;
+
+  if(is_empty(stack))
   {
     printf("Stack underflow\n");
-    return -1;
   }
   else{
-    return stack->top--;
+    printf("Remove %d\n",stack->top->element );
+    aux = stack->top;
+    stack->top = stack->top->next;
+    return (aux->next == NULL);
   }
+}
 
+int peek(STACK *stack)
+{
+  if(is_empty(stack))
+  {
+    printf("Stack underflow\n");
+  }
+  else
+  {
+    printf("%d\n",stack->top->element );
+    return (stack->top);
+  }
+}
+
+void printStack(NODE *node)
+{
+  if(node != NULL)
+  {
+    printf("%d ",node->element);
+    printStack(node->next);
+  }
 }
 
 int main()
 {
+  int n,i,e,rm;
+  printf("Insert a number of elements\n");
+  scanf("%d",&n);
+
+  STACK *new = createStack();
+
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d",&e);
+    push(new, e);
+  }
+
+  printf("Pop: ");
+  pop(new);
+
+  printf("\nPeek: ");
+  peek(new);
+
+  printf("\nThe stack\n");
+  printStack(new->top);
 
   return 0;
 }
