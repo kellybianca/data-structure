63bc2b818c9a818408d0e9f9f03de894343bfdf6
Kelly Bianca <41022649+kellybianca@users.noreply.github.com>
2019-08-02 10:49:45 -0300

    Update graph_bfs.c

--- Graph/graph_bfs.c
+++ Graph/graph_bfs.c
@@ -1,143 +1,204 @@
-    #include <stdio.h>
-    #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-    #define MAX 20
+#define MAX 10000000
 
-    typedef struct adj_list
+typedef struct node
+{
+    int item;
+    struct node *next;
+} NODE;
+
+typedef struct queue
+{
+    NODE *head;
+    NODE *tail;
+    int size;
+} QUEUE;
+
+typedef struct adj_list
+{
+    NODE *front;
+} ADJ_LIST;
+
+typedef struct graph
+{
+    ADJ_LIST *vertices[MAX];
+    int visited[MAX];
+} GRAPH;
+
+NODE *createNode(int item)
+{
+    NODE *newNode = (NODE *)malloc(sizeof(NODE));
+    newNode->item = item;
+    newNode->next = NULL;
+    return newNode;
+}
+
+QUEUE *create_queue()
+{
+    QUEUE *new = (QUEUE *)malloc(sizeof(QUEUE));
+    new->head = NULL;
+    new->tail = NULL;
+    new->size = 0;
+    return new;
+}
+
+GRAPH *create_graph()
+{
+    GRAPH *new = (GRAPH *)malloc(sizeof(GRAPH));
+    int i;
+    for (i = 0; i < MAX; i++)
     {
-        int item;
-        struct adj_list *next;
-    } ADJ_LIST;
-
-    typedef struct graph
-    {
-        ADJ_LIST *vertices[MAX];
-        int visited[MAX];
-    } GRAPH;
-
-    typedef struct queue
-    {
-        int current_size;
-        int head;
-        int tail;
-        int itens[30];
-    } QUEUE;
-
-    QUEUE *create_queue()
-    {
-        QUEUE *new = (QUEUE *)malloc(sizeof(QUEUE));
-        new->current_size = 0;
-        new->head = 0;
-        new->tail = MAX - 1;
-        return new;
+        new->vertices[i] = NULL;
+        new->visited[i] = 0;
     }
+    return new;
+}
 
-    GRAPH *create_graph()
+ADJ_LIST *create_adj_list(int item)
+{
+    ADJ_LIST *new = (ADJ_LIST *)malloc(sizeof(ADJ_LIST));
+    new->front = (NODE *)malloc(sizeof(NODE));
+    new->front->item = item;
+    new->front->next = NULL;
+    return new;
+}
+
+void enqueue(QUEUE *queue, int item)
+{
+    NODE *newNode = createNode(item);
+    if (queue->size == 0)
     {
-        GRAPH *new = (GRAPH *)malloc(sizeof(GRAPH));
-        int i;
-        for (i = 0; i < MAX; i++)
-        {
-            new->vertices[i] = NULL;
-            new->visited[i] = 0;
-        }
-        return new;
+        queue->head = newNode;
+        queue->tail = newNode;
+        queue->size++;
+        return;
     }
+    queue->tail->next = newNode;
+    queue->tail = newNode;
+    queue->size++;
+    return;
+}
 
-    ADJ_LIST *create_adj_list(int item)
+int dequeue(QUEUE *queue)
+{
+    if (!queue->size)
     {
-        ADJ_LIST *new = (ADJ_LIST *)malloc(sizeof(ADJ_LIST));
-        new->item = item;
-        new->next = NULL;
-        return new;
+        printf("0 elementos\n");
+        return 0;
     }
-
-    int is_empty(QUEUE *queue)
+    if (queue->size == 1)
     {
-        return (queue->itens == 0);
+        queue->size--;
+        NODE *a = queue->head;
+        queue->head = NULL;
+        queue->tail = NULL;
+        int value = a->item;
+        free(a);
+        return value;
     }
+    NODE *aux = queue->head;
+    int value = aux->item;
 
-    void add_edge(GRAPH *graph, int v1, int v2)
+    queue->head = aux->next;
+    queue->size--;
+    free(aux);
+    return value;
+}
+
+int is_empty(QUEUE *queue)
+{
+    return (queue->size == 0);
+}
+
+//directed
+void add_edge(GRAPH *graph, int v1, int v2)
+{
+    if(graph->vertices[v1] == NULL)
     {
-        ADJ_LIST *vertex = create_adj_list(v2);
-        vertex->next = graph->vertices[v1];
-        graph->vertices[v1] = vertex;
-
-        //undirected
-        vertex = create_adj_list(v1);
-        vertex->next = graph->vertices[v2];
-        graph->vertices[v2] = vertex;
+        graph->vertices[v1] = create_adj_list(v2);
     }
-
-    void enqueue(QUEUE *queue, int item)
+    else
     {
-        if (queue->current_size >= MAX)
-        {
-            printf("Queue Overflow\n");
-        }
-        else
-        {
-            queue->tail = (queue->tail + 1) % MAX;
-            queue->itens[queue->tail] = item;
-            queue->current_size++;
-        }
-    }
-
-    int dequeue(QUEUE *queue)
-    {
-        if (queue->head >= MAX)
-        {
-            printf("Queue Overflow\n");
-            return -1;
-        }
-        else
-        {
-            int dequeue = queue->itens[queue->head];
-            queue->head = (queue->head + 1) % MAX;
-            queue->current_size--;
-            return dequeue;
-        }
-    }
-
-    void bfs(GRAPH *graph, int source)
-    {
-        //printf("oi");
-        QUEUE *queue = create_queue();
+        NODE *new = createNode(v2);
+        NODE *new2 = graph->vertices[v1]->front;
+        new->next = graph->vertices[v1]->front;
+        graph->vertices[v1]->front = new;
+   }
     
-        int dequeued;
-        ADJ_LIST *adj_list = graph->vertices[source];
-        graph->visited[source] = 1;
-        enqueue(queue,source);
+}
 
+void bfs(GRAPH *graph, int source)
+{
+    QUEUE *queue = create_queue();
 
-        while (!is_empty(queue))
+    int dequeued;
+    NODE *node = graph->vertices[source]->front;
+    graph->visited[source] = 1;
+    enqueue(queue, source);
+
+    while (!is_empty(queue))
+    {
+        dequeued = dequeue(queue);
+        if(graph->vertices[dequeued] == NULL)
         {
-            dequeued = dequeue(queue);
-            adj_list = graph->vertices[dequeued];
-            while (adj_list != NULL)
+            continue;
+        }
+        node = graph->vertices[dequeued]->front;
+        while (node != NULL)
+        {
+            if (graph->visited[node->item] == 0)
             {
-                if (!graph->vertices[adj_list->item])
-                {
-                    printf("%d", adj_list->item);
-                    graph->visited[adj_list->item] = 1;
-                    enqueue(queue, adj_list->item);
-                }
-                adj_list = adj_list->next;
+                enqueue(queue, node->item);
+                graph->visited[node->item] = 1;
             }
+            node = node->next;
         }
     }
+}
 
-    int main()
+void printGraph(GRAPH *graph)
+{
+    int i;
+    NODE *aux;
+    for (i = 0; i < MAX; i++)
     {
-        GRAPH *graph = create_graph();
+        if(graph->vertices[i] == NULL)
+        {
+            continue;
+        }
+        aux = graph->vertices[i]->front;
+        printf("Vertex: %d", i);
+        while (aux != NULL)
+        {
+            printf("%d => ", aux->item);
+            aux = aux->next;
+        }
+        printf("\n");
 
-        add_edge(graph, 1, 2);
-        add_edge(graph, 1, 5);
-        add_edge(graph, 2, 5);
-        add_edge(graph, 2, 3);
-        add_edge(graph, 2, 4);
-        add_edge(graph, 3, 4);
-        add_edge(graph, 4, 5);
-
-        bfs(graph, 5);
     }
+}
+
+void add_undirected(GRAPH *graph, int v1, int v2)
+{
+    add_edge(graph, v1,v2);
+    add_edge(graph,v2,v1);
+}
+
+int main()
+{
+    GRAPH *graph = create_graph();
+
+    add_undirected(graph, 1, 2);
+    add_undirected(graph, 1, 5);
+    add_undirected(graph, 2, 5);
+    add_undirected(graph, 2, 3);
+    add_undirected(graph, 2, 4);
+    add_undirected(graph, 3, 4);
+    add_undirected(graph, 4, 5);
+
+    bfs(graph, 5);
+
+    printGraph(graph);
+}
